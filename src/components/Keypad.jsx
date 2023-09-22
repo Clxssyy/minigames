eed82fe17386ed246ef0acafa2a1fdf06bf29adc
Michael Connolly <mpc63@uakron.edu>
2023-09-22 18:56:13 -0400

    Refactor Keypad component

--- src/components/Keypad.jsx
+++ src/components/Keypad.jsx
@@ -3,103 +3,41 @@ const Keypad = (props) => {
     <div className='grid grid-cols-4'>
       {props.numbers.map((number, index) => {
         const row = Math.floor(index / 4);
+        let color;
         if (row % 2 === 0) {
           if (number % 2 === 0) {
-            return (
-              <button
-                onClick={() => {
-                  if (props.start) {
-                    if (number === props.activeNumber) {
-                      document.getElementById(number).style.backgroundColor =
-                        'green';
-                      props.setActiveNumber((prevNumber) => prevNumber + 1);
-                    } else {
-                      document.getElementById(number).style.backgroundColor =
-                        'red';
-                      props.setErrors((prevErrors) => prevErrors + 1);
-                    }
-                  }
-                }}
-                className={`bg-zinc-800 p-4 text-green-900 font-bold text-center`}
-                key={number}
-                id={number}
-              >
-                {number}
-              </button>
-            );
+            color = '800';
           } else {
-            return (
-              <button
-                onClick={() => {
-                  if (props.start) {
-                    if (number === props.activeNumber) {
-                      document.getElementById(number).style.backgroundColor =
-                        'green';
-                      props.setActiveNumber((prevNumber) => prevNumber + 1);
-                    } else {
-                      document.getElementById(number).style.backgroundColor =
-                        'red';
-                      props.setErrors((prevErrors) => prevErrors + 1);
-                    }
-                  }
-                }}
-                className={`bg-zinc-900 p-4 text-green-900 font-bold text-center`}
-                key={number}
-                id={number}
-              >
-                {number}
-              </button>
-            );
+            color = '900';
           }
         } else {
-          if (number % 2 !== 0) {
-            return (
-              <button
-                onClick={() => {
-                  if (props.start) {
-                    if (number === props.activeNumber) {
-                      document.getElementById(number).style.backgroundColor =
-                        'green';
-                      props.setActiveNumber((prevNumber) => prevNumber + 1);
-                    } else {
-                      document.getElementById(number).style.backgroundColor =
-                        'red';
-                      props.setErrors((prevErrors) => prevErrors + 1);
-                    }
-                  }
-                }}
-                className={`bg-zinc-800 p-4 text-green-900 font-bold text-center`}
-                key={number}
-                id={number}
-              >
-                {number}
-              </button>
-            );
+          if (number % 2 === 0) {
+            color = '900';
           } else {
-            return (
-              <button
-                onClick={() => {
-                  if (props.start) {
-                    if (number === props.activeNumber) {
-                      document.getElementById(number).style.backgroundColor =
-                        'green';
-                      props.setActiveNumber((prevNumber) => prevNumber + 1);
-                    } else {
-                      document.getElementById(number).style.backgroundColor =
-                        'red';
-                      props.setErrors((prevErrors) => prevErrors + 1);
-                    }
-                  }
-                }}
-                className={`bg-zinc-900 p-4 text-green-900 font-bold text-center`}
-                key={number}
-                id={number}
-              >
-                {number}
-              </button>
-            );
+            color = '800';
           }
         }
+        return (
+          <button
+            onClick={() => {
+              if (props.start) {
+                if (number === props.activeNumber) {
+                  document.getElementById(number).style.backgroundColor =
+                    'green';
+                  props.setActiveNumber((prevNumber) => prevNumber + 1);
+                } else {
+                  document.getElementById(number).style.backgroundColor = 'red';
+                  props.setErrors((prevErrors) => prevErrors + 1);
+                }
+              }
+            }}
+            className={`bg-zinc-${color} p-4 text-green-900 font-bold text-center`}
+            key={number}
+            id={number}
+          >
+            {number}
+          </button>
+        );
       })}
     </div>
   );
