4366df8fe5423ac3441650389be88584d87c6db6
Michael Connolly <mpc63@uakron.edu>
2023-09-24 23:53:22 -0400

    Update home page

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,26 @@
-import Keypad from '../components/Keypad';
+import Link from 'next/link';
 
 export default function Home() {
   return (
     <main className='min-h-screen flex flex-col gap-2 place-items-center bg-stone-500'>
-      <div className='p-4'>
-        <h1 className='text-4xl font-bold text-center'>Keypad Decryption</h1>
+      <div className='p-2'>
+        <h1 className='text-4xl font-bold text-center'>Classy Minigames</h1>
       </div>
-      <Keypad />
+      <div>
+        <ul className='list-disc'>
+          <li>
+            <Link href='/minigames/keypad-decryption'>Keypad Decryption</Link>
+          </li>
+        </ul>
+      </div>
+      <div className='p-2 flex flex-col place-items-center'>
+        <h2 className='text-2xl font-bold'>Coming Soon</h2>
+        <ul className='list-disc'>
+          <li>
+            <Link href='/'>temp</Link>
+          </li>
+        </ul>
+      </div>
     </main>
   );
 }
