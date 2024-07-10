7c760faafa2a5523c7a40e79a63b1852ca467878
Michael Connolly <mpc63@uakron.edu>
2024-07-09 23:17:37 -0400

    Update page styling

--- src/app/minigames/keypad-decryption/page.jsx
+++ src/app/minigames/keypad-decryption/page.jsx
@@ -2,9 +2,11 @@ import Keypad from '@/components/Keypad';
 
 const KeypadPage = () => {
   return (
-    <main className='min-h-screen flex flex-col gap-2 place-items-center bg-stone-500'>
+    <main className='min-h-screen flex flex-col gap-2 place-items-center wave2-bg'>
       <div className='p-4'>
-        <h1 className='text-4xl font-bold text-center'>Keypad Decryption</h1>
+        <h1 className='text-4xl text-white font-bold text-center'>
+          Keypad Decryption
+        </h1>
       </div>
       <Keypad />
     </main>
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -2,25 +2,16 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className='min-h-screen flex flex-col gap-2 place-items-center bg-stone-500'>
-      <div className='p-2'>
-        <h1 className='text-4xl font-bold text-center'>Classy Minigames</h1>
+    <main className='min-h-screen flex wave-bg'>
+      <div className='w-1/2 flex flex-col gap-2 place-items-center'>
+        <h1 className='text-white text-4xl font-bold'>Classy Minigames</h1>
+        <div className='p-2 bg-[#FBAE3C] rounded'>
+          <Link href='/minigames/keypad-decryption' className='font-bold'>
+            Keypad Decryption
+          </Link>
+        </div>
       </div>
-      <div>
-        <ul className='list-disc'>
-          <li>
-            <Link href='/minigames/keypad-decryption'>Keypad Decryption</Link>
-          </li>
-        </ul>
-      </div>
-      <div className='p-2 flex flex-col place-items-center'>
-        <h2 className='text-2xl font-bold'>Coming Soon</h2>
-        <ul className='list-disc'>
-          <li>
-            <Link href='/minigames/memory'>Memory</Link>
-          </li>
-        </ul>
-      </div>
+      <div className='w-1/2'></div>
     </main>
   );
 }
